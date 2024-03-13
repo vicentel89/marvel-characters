789c1091152d850f79a9c57ba3c4192d8739bc21
Vicente Losada <vi_losada@hotmail.com>
2024-03-13 08:52:13 +0100

    Avoid refetching on favorite click in home page

--- src/modules/characters/infrastructure/methods/search-many.ts
+++ src/modules/characters/infrastructure/methods/search-many.ts
@@ -1,3 +1,5 @@
+import { unstable_cache } from 'next/cache';
+
 import { SearchParams } from '@/modules/core/domain/search-params';
 import { BaseEndpointParams } from '@/modules/core/infrastructure/marvel-api/endpoint-params';
 import { generateMarvelApiUrl } from '@/modules/core/infrastructure/marvel-api/generate-api-url';
@@ -8,12 +10,28 @@ interface CharacterEndpointParams extends BaseEndpointParams {
   nameStartsWith?: string;
 }
 
-export async function searchMany({ search, ...searchParams }: SearchParams) {
+export async function searchMany(searchParams: SearchParams) {
+  const cachedCharacters = unstable_cache(
+    async (searchParams) => fetchSearchMany(searchParams),
+    ['search-characters'],
+    {
+      revalidate: 60 * 60 * 24 * 3, // 3 days
+      tags: ['search-characters'],
+    }
+  );
+
+  const result = await cachedCharacters(searchParams);
+  const allCharacters = markFavoriteCharacters(result);
+
+  return allCharacters;
+}
+
+export async function fetchSearchMany({ search, ...searchParams }: SearchParams) {
   const params: CharacterEndpointParams = { limit: 50, ...searchParams };
   if (search) params.nameStartsWith = search;
 
   const apiUrl = generateMarvelApiUrl('characters', params);
-  const data = await fetchAndParse(apiUrl, { cache: 'no-store' });
+  const data = await fetchAndParse(apiUrl);
   const mappedCharacters = mapApiResponseToCharacter(data.results);
   const characters = markFavoriteCharacters(mappedCharacters);
 
